f5f095fa098950d8bf24d4e98e1a9a93596b1fda
Muzammil K <muzammil_kamaly93@live.se>
2023-01-17 11:01:43 +0100

    Update

--- Star-Wars-API/source_to_raw/Muzammil_Kamaly/Muzammil_Code.py
+++ Star-Wars-API/source_to_raw/Muzammil_Kamaly/Muzammil_Code.py
@@ -2,3 +2,13 @@ import pandas
 import requests
 
 #Does this work?
+
+
+#Url = http://swapi.dev/api/planets
+
+response = requests.get("http://swapi.dev/api/planets")
+
+# Convert the response data to a JSON object
+data = response.json()
+
+print(data)
