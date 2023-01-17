3b8f87b139cf6e888c0838f2a318b1390733a7b9
MrSushi201 <santi@taweesamarn.com>
2023-01-17 12:12:20 +0100

    # 1 - dirs & data.json

--- Star-Wars-API/source_to_raw/source_to_raw.py
+++ Star-Wars-API/source_to_raw/source_to_raw.py
@@ -1,1 +1,27 @@
+import os
+import pandas as pd
 import requests
+
+# Create dir references
+WORKDIR = os.path.dirname(os.path.realpath(__file__))
+print(WORKDIR)
+
+ROOTPATH = os.path.abspath(os.path.join(WORKDIR, os.pardir))
+print(ROOTPATH)
+
+source_path = ROOTPATH + "/data/testing/raw/"
+print(source_path)
+
+# # If dir not exist, otherwise #
+os.makedirs(source_path)
+
+# API Base URL
+# URL = "https://swapi.dev/api/"
+
+response = requests.get("http://swapi.dev/api/planets/")
+
+data = response.json()
+
+df = pd.DataFrame(data)
+
+df.to_json(source_path + "data.json", orient = "records")
